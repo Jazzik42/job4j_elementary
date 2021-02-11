5d762136dfb288456ce7422a68ce3186cdbd2ac9
Jazzik42 <rmuzrrr@gmail.com>
2021-02-11 22:56:32 +0500

    2. Идентичные тексты [#424347]

---
+++ src/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] duplicate = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String orig : origin) {
+            check.add(orig);
+        }
+        for (String dupl : duplicate) {
+            if (!check.contains(dupl)) {
+                rsl = false;
+            break;
+        }
+    }
+        return rsl;
+    }
+}
+
