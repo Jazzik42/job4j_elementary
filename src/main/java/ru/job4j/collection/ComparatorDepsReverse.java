35ee0f6b2eb8380855b148d95261055002426714
Jazzik42 <rmuzrrr@gmail.com>
2021-02-22 13:33:55 +0500

    2. Отсортировать департаменты [#424323]

--- src/main/java/ru/job4j/collection/ComparatorDepsReverse.java
+++ src/main/java/ru/job4j/collection/ComparatorDepsReverse.java
@@ -6,10 +6,7 @@ public class ComparatorDepsReverse implements Comparator<String> {
     @Override
     public int compare(String one, String two) {
         int a = two.split("/")[0].compareTo(one.split("/")[0]);
-        if (a == 0) {
-            return one.compareTo(two);
-        } else {
-            return a;
+        return a == 0 ? one.compareTo(two) : a;
         }
     }
-}
+
