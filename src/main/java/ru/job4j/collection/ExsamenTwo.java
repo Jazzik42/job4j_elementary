150b48ba66f2f7298892b7b780aeafdba133b849
Jazzik42 <rmuzrrr@gmail.com>
2021-03-29 17:53:01 +0500

    1. Экзамен по коллекциям. 2. Проверить две строчки на идентичность.

--- src/main/java/ru/job4j/collection/ExsamenTwo.java
+++ src/main/java/ru/job4j/collection/ExsamenTwo.java
@@ -2,6 +2,7 @@ package ru.job4j.collection;
 
 
     import java.util.HashMap;
+    import java.util.List;
     import java.util.function.Function;
     import java.util.stream.Collectors;
     import java.util.stream.Stream;
@@ -9,13 +10,21 @@ package ru.job4j.collection;
 public class ExsamenTwo {
         public static boolean eq(String left, String right) {
             HashMap<Character, Long> map = new HashMap<>();
-            HashMap<Character, Long> rightMap = new HashMap<>();
             Stream<Character> chars = left.chars().mapToObj(i -> (char) i);
-            Stream<Character> rightChars = right.chars().mapToObj(i -> (char) i);
             map.putAll(chars.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
-            rightMap.putAll(rightChars.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())));
-
-            return map.equals(rightMap);
-        }
+            List<Character> rightChars = right.chars()
+                    .mapToObj(i -> Character.valueOf((char) i))
+                    .collect(Collectors.toList());
+            for (char i : rightChars) {
+                if (!map.containsKey(i)) {
+                    return false;
+                } else if (map.containsKey(i) && map.get(i) == 1) {
+                        map.remove(i);
+                    } else if (map.containsKey(i) && map.get(i) > 1) {
+                        map.put(i, map.get(i) - 1);
+                    }
+                    }
+            return map.isEmpty();
+            }
     }
 
