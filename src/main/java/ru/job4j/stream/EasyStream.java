28947f00bce7c39c0dcfb820882fd164eef9d926
Jazzik42 <rmuzrrr@gmail.com>
2021-03-30 14:46:51 +0500

    1. Экзамен по Stream.

---
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,75 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EasyStream {
+    private List<Integer> source;
+    private Function<Integer, Integer> fun;
+    private Predicate<Integer> predicate;
+
+    public static class Builder {
+        private List<Integer> source;
+        private Function<Integer, Integer> fun;
+        private Predicate<Integer> predicate;
+
+        private Builder buildSource(List<Integer> source) {
+            this.source = source;
+            return this;
+        }
+
+        private Builder buildFun(Function<Integer, Integer> fun) {
+            this.fun = fun;
+            return this;
+        }
+
+        private Builder buildPredicate(Predicate<Integer> predicate) {
+            this.predicate = predicate;
+            return this;
+        }
+
+        private EasyStream build() {
+            EasyStream easyStream = new EasyStream();
+            easyStream.fun = fun;
+            easyStream.source = source;
+            easyStream.predicate = predicate;
+            return easyStream;
+        }
+    }
+
+        public static EasyStream of(List<Integer> source) {
+            return new Builder()
+                    .buildSource(source)
+                    .build();
+
+        }
+
+        public EasyStream map(Function<Integer, Integer> fun) {
+            List<Integer> bufferSource = new ArrayList<>();
+            for (int sources : source) {
+                bufferSource.add(fun.apply(sources));
+            }
+            return new Builder()
+                    .buildSource(bufferSource)
+                    .build();
+        }
+
+        public EasyStream filter(Predicate<Integer> fun) {
+            List<Integer> bufferSource = new ArrayList<>();
+            for (int i = 0; i < source.size(); i++) {
+                if (fun.test(source.get(i))) {
+                    bufferSource.add(source.get(i));
+                }
+            }
+            return new Builder()
+                    .buildSource(bufferSource)
+                    .build();
+        }
+
+        public List<Integer> collect() {
+        return source;
+        }
+    }
+
